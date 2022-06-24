cd5f4bd3bad460ac191c4395eda79b9a6d12a4ee
bumahkib7 <93518580+bumahkib7@users.noreply.github.com>
2022-06-24 19:59:17 +0200

    initial Commit

---
+++ src/main/java/com/example/adaccarservices/AdacCarServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.adaccarservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdacCarServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdacCarServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/adaccarservices/AdacCarServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adaccarservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdacCarServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
